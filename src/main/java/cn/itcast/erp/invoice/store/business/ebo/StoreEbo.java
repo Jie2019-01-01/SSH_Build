73f8c17950c2801b1b47b90112f966c79d22f10f
Jie2019-01-01 <wy12012156@163.com>
2020-02-15 21:06:29 +0800

    仓库模块-新增、显示初始化

---
+++ src/main/java/cn/itcast/erp/invoice/store/business/ebo/StoreEbo.java
@@ -0,0 +1,45 @@
+package cn.itcast.erp.invoice.store.business.ebo;
+
+import java.util.List;
+
+import cn.itcast.erp.invoice.store.business.ebi.StoreEbi;
+import cn.itcast.erp.invoice.store.dao.dao.StoreDao;
+import cn.itcast.erp.invoice.store.vo.StoreModel;
+import cn.itcast.erp.invoice.store.vo.StoreQueryModel;
+
+public class StoreEbo implements StoreEbi{
+	
+	private StoreDao storeDao;
+	public void setStoreDao(StoreDao storeDao) {
+		this.storeDao = storeDao;
+	}
+
+	public void save(StoreModel sm) {
+		storeDao.save(sm);
+	}
+
+	public void update(StoreModel sm) {
+		storeDao.update(sm);
+	}
+
+	public void delete(StoreModel sm) {
+		storeDao.delete(sm);
+	}
+
+	public StoreModel get(Long uuid) {
+		return storeDao.get(uuid);
+	}
+
+	public List<StoreModel> getAll() {
+		return storeDao.getAll();
+	}
+
+	public List<StoreModel> getAll(StoreQueryModel sqm, Integer pageNum,Integer pageCount) {
+		return storeDao.getAll(sqm,pageNum,pageCount);
+	}
+
+	public Integer getCount(StoreQueryModel sqm) {
+		return storeDao.getCount(sqm);
+	}
+
+}
